a85bad0ba20e8559f2db13334e4288c432f0ed1a
Praveen <fattesinha.sawant@outlook.com>
2022-09-22 14:40:06 +0530

    Resource group module added

---
+++ resource-group/variables.tf
@@ -0,0 +1,24 @@
+variable "name" {
+  type = string
+  description = "The resource group name"
+}
+
+variable "location" {
+  type = string
+  description = "The location where the resource needs to be created"
+}
+
+variable "project" {
+  type = string
+  description = "Name of the project"
+}
+
+variable "lob" {
+    type = string
+    ddescription = "Name of Line of Bussiness"  
+}
+
+variable "zone" {
+    type = string
+    description = "value"  
+}
