c4921b2cf4aa0e3d3c3c5539a259a286b8e41133
Praveen <fattesinha.sawant@outlook.com>
2022-09-23 11:58:47 +0530

    basic change in outputs

--- log-workspace/outputs.tf
+++ log-workspace/outputs.tf
@@ -1,7 +1,23 @@
 output "name" {
-  value = azurerm_resource_group.resource_group.name
+  value = azurerm_log_analytics_workspace.law.name
 }
 
 output "location" {
-  value = azurerm_resource_group.resource_group.location
+  value = azurerm_log_analytics_workspace.law.location
+}
+
+output "id" {
+  value = azurerm_log_analytics_workspace.law.id
+}
+
+output "workspace_id" {
+  value = azurerm_log_analytics_workspace.law.workspace_id
+}
+
+output "primary_shared_key" {
+  value = azurerm_log_analytics_workspace.law.primary_shared_key
+}
+
+output "secondary_shared_key" {
+  value = azurerm_log_analytics_workspace.law.secondary_shared_key
 }
