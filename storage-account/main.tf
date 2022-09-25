b4632e5b154942630524e869fe9ebd653a1153c3
Praveen <fattesinha.sawant@outlook.com>
2022-09-25 16:45:32 +0530

    storage account changed

--- storage-account/main.tf
+++ storage-account/main.tf
@@ -12,7 +12,7 @@ resource "azurerm_storage_account" "storage_account" {
   resource_group_name      = var.resource_group_name
   location                 = var.location
   account_tier             = var.account_tier
-  account_replication_type = var.account_tier
+  account_replication_type = var.account_replication_type
   account_kind             = "StorageV2"
   enable_https_traffic_only = true
 
