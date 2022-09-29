a4700064c65cb177b97c99bb2b1a83bcb5bdad20
Praveen <fattesinha.sawant@outlook.com>
2022-09-29 11:45:42 +0530

    subnet module edited

--- subnet/main.tf
+++ subnet/main.tf
@@ -1,8 +1,8 @@
 
 resource "azurerm_subnet" "subnet" {
-  name                 = "${terraform.workspace}-${var.project}-${var.zone}-${var.lob}-${var.location}-rg"
+  name                 = "${terraform.workspace}-${var.project}-${var.zone}-${var.lob}-${var.location}-sn"
   resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.example.name
+  virtual_network_name = var.vnet_name
   address_prefixes     = [cidrsubnet(var.vnet_cidr, var.subnet_cidr_size, var.subnet_cidr_netnum)]
   service_endpoints = var.service_endpoints
   enforce_private_link_endpoint_network_policies = var.enforce_private_link_endpoint_network_policies
